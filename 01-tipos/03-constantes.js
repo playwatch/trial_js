be7e71cb3e4f654c30037f8bdc24a7dcc150be40
kenyohero <xrygan@gmail.com>
2024-09-11 19:23:03 -0500

    V 5

--- 01-tipos/03-constantes.js
+++ 01-tipos/03-constantes.js
@@ -1,3 +1,3 @@
 const nombre2 = "hola w";
-nombre2 = "happy";
+//nombre2 = "happy";
 console.log(nombre2);
--- 01-tipos/04-tipado-dinamico.js
+++ 01-tipos/04-tipado-dinamico.js
@@ -1,12 +1,12 @@
 let numero2 = 42;
 let nombre3 = "h w";
-let verdadero = true;
-let undef;
+let verdadero2 = true;
+let undef2;
 let nula = null;
 
 numero2 = 53;
 console.log(typeof numero2);
 console.log(typeof nombre3);
-console.log(typeof verdadero);
-console.log(typeof undef);
+console.log(typeof verdadero2);
+console.log(typeof undef2);
 console.log(typeof nula);
